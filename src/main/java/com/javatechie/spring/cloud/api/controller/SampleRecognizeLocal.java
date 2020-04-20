882c4dbb5f31a4b82e06af74b43db906c96ae0da
luis taborda <luis.taborda@wasys.com.br>
2020-04-20 14:54:13 -0300

    conversor mp3 e nova class AudioFile que extende funções do audio

--- src/main/java/com/javatechie/spring/cloud/api/controller/SampleRecognizeLocal.java
+++ src/main/java/com/javatechie/spring/cloud/api/controller/SampleRecognizeLocal.java
@@ -3,39 +3,30 @@ package com.javatechie.spring.cloud.api.controller;
 import com.google.cloud.speech.v1.*;
 import com.google.protobuf.ByteString;
 
-import java.io.IOException;
+
+import javax.sound.sampled.*;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
 public class SampleRecognizeLocal {
-    /**
-     * Transcribe a short audio file using synchronous speech recognition
-     *
-     * @param localFilePath Path to local audio file, e.g. /path/audio.wav
-     */
-    public static String transcript(String localFilePath) {
+
+    public static String transcript(AudioFile audioFile) {
         try (SpeechClient speechClient = SpeechClient.create()) {
 
-            // The language of the supplied audio
-            String languageCode = "pt-BR";
-
-            // Sample rate in Hertz of the audio data sent
-            int sampleRateHertz = 44100;
-
-            // Encoding of audio data sent. This sample sets this explicitly.
-            // This field is optional for FLAC and WAV audio formats.
             RecognitionConfig.AudioEncoding encoding = RecognitionConfig.AudioEncoding.LINEAR16 ;
-            RecognitionConfig config = RecognitionConfig.newBuilder().setLanguageCode(languageCode).setSampleRateHertz(sampleRateHertz).setEncoding(encoding).build();
-            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(AudioToByteString(localFilePath)).build();
+            RecognitionConfig config = RecognitionConfig.newBuilder().setLanguageCode(audioFile.getLanguageCode()).setSampleRateHertz(audioFile.getSampleRateHertz()).setEncoding(encoding).build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(AudioToByteString(audioFile.getFile())).build();
 
-            //Passa o audio em ByteString para API do google processar.
             RecognizeRequest request = RecognizeRequest.newBuilder().setConfig(config).setAudio(audio).build();
 
             RecognizeResponse response = speechClient.recognize(request);
 
             for (SpeechRecognitionResult result : response.getResultsList()) {
-                // First alternative is the most probable result
+
                 SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
                 System.out.printf("Transcript: %s\n", alternative.getTranscript());
                 return alternative.getTranscript();
@@ -47,15 +38,10 @@ public class SampleRecognizeLocal {
         return "The file could not be processed, try again by changing the file format.";
     }
 
-    /**
-     * Está função retorna um arquvio de audio em ByteString, padrão da api google cloud plataform
-     *
-     * @param localFilePath Path to local audio file, e.g. /path/audio.wav
-     */
-    public static ByteString AudioToByteString(String localFilePath) throws IOException {
-        Path path = Paths.get(localFilePath);
+    public static ByteString AudioToByteString(File file) throws IOException {
+
+        Path path = Paths.get(file.getAbsolutePath());
         byte[] data = Files.readAllBytes(path);
         return ByteString.copyFrom(data);
     }
-
 }
--- src/main/java/com/javatechie/spring/cloud/api/controller/SpeechToTextController.java
+++ src/main/java/com/javatechie/spring/cloud/api/controller/SpeechToTextController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.sound.sampled.spi.AudioFileWriter;
+
 @Controller
 public class SpeechToTextController {
 
+     private final String localpath = "C:\\Users\\usuario\\Documents\\workspace\\speech-to-text\\src\\main\\resources\\audios\\9b2g.wav";
 
     @RequestMapping(value="/speech", method = RequestMethod.GET)
     public String speech( @RequestParam( name = "transcript", required = false, defaultValue = "Error Failed load audio") String transcript, Model model) throws Exception {
-        transcript = SampleRecognizeLocal.transcript("C:\\Users\\usuario\\Documents\\workspace\\speech-to-text\\src\\main\\resources\\audios\\9b2g.wav");
+
+        AudioFile audioFile = AudioUtilities.audioToSpeech(localpath);
+
+        //transcript = SampleRecognizeLocal.transcript(localpath);
+        transcript = SampleRecognizeLocal.transcript(audioFile);
         model.addAttribute("transcript",transcript);
         return "speech";
     }
